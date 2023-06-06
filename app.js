ad5915e0f6904b15fc161b1031c006e28d78278b
pranav343 <pranav.dhiman334433@gmail.com>
2023-06-06 14:12:09 +0530

    commit done

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const request=require("request");
 const https=require("https"); 
 
 const app=express();
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public")); 
 
+// here we use express.static and the we write the name of the folder here it is public
+// in this folder all the static things like css , images and etc should be there.
+
+app.use(bodyParser.urlencoded({extended:true}));
+//------------till here it is common for all-----------------//
 app.get("/", function(req, res)
 {
     res.sendFile(__dirname+"/signup.html");
@@ -39,14 +43,14 @@ app.post("/", function(req,res)
     
     const url= "https://us21.api.mailchimp.com/3.0/lists/c01cf61293";
     
-    const options={
+    const options={   //this options tell that what to do like method as post or to get 
         method:"POST",
-        auth: "Pranav:5265b263eca4314b1b6aa54b65a3f4bb-us21"
+        auth: "Pranav:6b8f6b3b3f9d7d4d595bd80842e43150-us21"
 
 
     }
 
-    const request=https.request(url, options , function(response)
+    const request=https.request(url, options , function(response) //options here tell what to do like to post or to get the request
     {
         if (response.statusCode===200)
         {
@@ -66,7 +70,7 @@ app.post("/", function(req,res)
 });
 
 
-app.listen(process.env.PORT || 3000, function()
+app.listen(process.env.PORT || 3000, function() //for server hosting we put this
 {
     console.log("server is running on port 3000");
 })
@@ -74,4 +78,6 @@ app.listen(process.env.PORT || 3000, function()
 // api key-5265b263eca4314b1b6aa54b65a3f4bb-us21
 
 // audiance-id c01cf61293
+
+// api 6b8f6b3b3f9d7d4d595bd80842e43150-us21
  
